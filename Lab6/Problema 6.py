91d657fce399a3ac78ee3f62dc1ddbbc0724fcc8
cipriancus <cipriancus@gmail.com>
2016-11-23 19:53:14 +0200

    Update Problema 6.py

--- Lab6/Problema 6.py
+++ Lab6/Problema 6.py
@@ -1,8 +1,15 @@
 import re
+#nu e gata
+def replace_function(match):
+    string=match.group(0)
+    for iterator in range(0,len(string)):
+        if iterator % 2 == 1:
+            string=string[:iterator-1]+'*'+string[iterator+1:]
+    return string
 
-def cenzura(text):
-    string_vocale=re.findall("(\W[aeiou]\W)|(\W[aeiou]\w*[aeiou]\W)|(^[aeiou]\w*[aeiou]\W)|(\W[aeiou]\w*[aeiou]$)",text)
-    return string_vocale
+def get_text_cenzurat(text_string):
+    text_to_return = re.sub("(?<=\W)[aeiou][a-z0-9]*[aeiou](?=\W)", replace_function, text_string)
+    return text_to_return
 
 
-print(cenzura("aipria este smecher"))
+print(get_text_cenzurat("ipria este smecher"))
